a28bf58c7f4bae74bcd7b8ff8d8810654fee0a4c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-10-18 17:45:34 +0200

    style sections

--- cig/view.py
+++ cig/view.py
@@ -18,8 +18,10 @@ def layout(title: Optional[str], body: List[h]) -> h:
             h("link", rel="shortcut icon", href="/static/tuc/favicon.ico")
         ),
         h("body")(
-            h("img", src="/static/tuc/logo.svg"),
-            body
+            h("main")(
+                h("img", src="/static/tuc/logo.svg"),
+                body
+            )
         )
     )
 
@@ -38,10 +40,12 @@ def index() -> h:
 def login(*, lecture: Lecture, error: Optional[str] = None) -> h:
     return layout(lecture.title, [
         h("h1")(f"Register for the next ", h("em")(lecture.title), " lecture (step 1/3)"),
-        h("form", method="POST")(
-            error and h("p")(error),
-            h("input", type="email", placeholder="max.mustermann@tu-clausthal.de", name="email", required=True),
-            h("button", type="submit")("Send login link")
+        h("section")(
+            h("form", method="POST")(
+                error and h("p")(error),
+                h("input", type="email", placeholder="max.mustermann@tu-clausthal.de", name="email", required=True),
+                h("button", type="submit")("Send login link")
+            )
         ),
     ])
 
@@ -49,26 +53,28 @@ def login(*, lecture: Lecture, error: Optional[str] = None) -> h:
 def link_sent(*, lecture: Lecture) -> h:
     return layout(lecture.title, [
         h("h1")("Link sent (step 2/3)"),
-        h("p")("Check your inbox.")
+        h("section")(
+            h("p")("Check your inbox.")
+        )
     ])
 
 
 def register(*, lecture: Lecture, email: str, events: List[Registrations], admin: bool = False) -> h:
     return layout(lecture.title, [
         h("h1")("Register for the next ", h("em")(lecture.title), " lecture (step 3/3)"),
-        h("h2")("Your contact information"),
-        h("p")("You are logged in as ", h("strong")(email), "."),
-        h("p")("We do not need additional contact information at this time. But please keep your details updated with the Studentensekretariat."),
-        [
+        h("section")(
+            h("h2")("Your contact information"),
+            h("p")("You are logged in as ", h("strong")(email), "."),
+            h("p")("We do not need additional contact information at this time. But please keep your details updated with the Studentensekretariat.")
+        ),
+        h("section")(
             h("h2")("Signup not yet open"),
             h("p")("Singup opens on the day of each lecture.")
-        ] if not events else [
-            h("div")(
+        ) if not events else [
+            h("section")(
                 h("h2")(registrations.event.title, " (", registrations.event.date.strftime("%a, %d.%m."), ")"),
-                h("ul")(
-                    h("li")("Please reserve a seat only if you will physically attend this lecture in ", h("strong")(registrations.event.location), "."),
-                    h("li")("Please come only after you successfully reserved a seat. There are ", h("strong")(f"{registrations.event.seats} seats"), " in total.")
-                ),
+                h("p")("Please reserve a seat only if you will physically attend this lecture in ", h("strong")(registrations.event.location), "."),
+                h("p")("Please come only after you successfully reserved a seat. There are ", h("strong")(f"{registrations.event.seats} seats"), " in total."),
                 h("table")(
                     h("thead")(
                         h("tr")(
@@ -85,7 +91,7 @@ def register(*, lecture: Lecture, email: str, events: List[Registrations], admin
                             h("td")(row.n),
                             h("td")(row.name),
                             h("td")(
-                                row.time.strftime("Successfully registered %d.%m %H:%m") if row.n <= registrations.event.seats else "No seat was available. We will make sure to provide the lecture materials online."
+                                row.time.strftime("Successfully registered %d.%m. %H:%m" if row.n <= registrations.event.seats else "No seat was available at %d.%m. %H:%m. We will make sure to provide the lecture materials online.")
                             )
                         ) for row in registrations.rows() if row.name == email or admin
                     ])
