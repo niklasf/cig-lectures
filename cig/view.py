1386de7d8e0b05f54632c0de4d5390bea7633b3c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-10-21 00:23:04 +0200

    more trailing commas

--- cig/view.py
+++ cig/view.py
@@ -44,7 +44,7 @@ def index() -> Frag:
             h("li")(
                 h("a", href=url(name))(lecture.title)
             ) for name, lecture in cig.data.LECTURES.items()
-        )
+        ),
     ))
 
 
@@ -57,9 +57,9 @@ def login(*, lecture: Lecture, error: Optional[str] = None) -> Frag:
                 h("p")(
                     h("label", for_="email")("Email:"),
                     " ",
-                    h("input", type="email", placeholder="max.mustermann@tu-clausthal.de", id="email", name="email", required=True)
+                    h("input", type="email", placeholder="max.mustermann@tu-clausthal.de", id="email", name="email", required=True),
                 ),
-                h("p")(h("button", type="submit")("Send login link"))
+                h("p")(h("button", type="submit")("Send login link")),
             )
         )
     ))
@@ -72,8 +72,8 @@ def link_sent(*, lecture: Lecture, email_text: Optional[str]) -> Frag:
             h("p")("Check your inbox."),
             h("p")(
                 "Development mode enabled. This email would have been sent: ",
-                h("pre")(email_text)
-            ) if email_text else None
+                h("pre")(email_text),
+            ) if email_text else None,
         )
     ))
 
@@ -94,17 +94,17 @@ def register(*, lecture: Lecture, email: str, events: List[Registrations], admin
         h("section", klass="no-print")(
             h("h2")("Your contact information"),
             h("p")("You are logged in as ", h("strong")(email), "."),
-            h("p")("We do not need additional contact information at this time. But please keep your details updated with the Studentensekretariat.")
+            h("p")("We do not need additional contact information at this time. But please keep your details updated with the Studentensekretariat."),
         ),
         h("section")(
             h("h2")("Signup not open, yet"),
-            h("p")("Signup opens on the day of each lecture. Please come only after you have successfully reserved a seat.")
+            h("p")("Signup opens on the day of each lecture. Please come only after you have successfully reserved a seat."),
         ) if not events else [
             h("section", klass={
                 "not-today": registrations.event.date != today,
             })(
                 h("h2", id=f"event-{registrations.event.id}")(
-                    registrations.event.title, " (", registrations.event.date.strftime("%a, %d.%m."), ")"
+                    registrations.event.title, " (", registrations.event.date.strftime("%a, %d.%m."), ")",
                 ),
                 h("p")("Please reserve a seat only if you will physically attend this lecture in ", h("strong")(registrations.event.location), "."),
                 h("p")("Please come only after you successfully reserved a seat. There are ", h("strong")(f"{registrations.event.seats} seats"), " in total."),
@@ -114,7 +114,7 @@ def register(*, lecture: Lecture, email: str, events: List[Registrations], admin
                             h("th")("Seat"),
                             h("th")("Name"),
                             h("th")("Status"),
-                            h("th", klass="no-print")("Admin") if admin else None
+                            h("th", klass="no-print")("Admin") if admin else None,
                         )
                     ),
                     h("tbody")([
@@ -125,22 +125,22 @@ def register(*, lecture: Lecture, email: str, events: List[Registrations], admin
                             h("td")(modifier(row)(f"#{row.n}") if row.n is not None else ""),
                             h("td")(modifier(row)(row.name)),
                             h("td")(
-                                "Reservation deleted by admin" if row.deleted else row.time.strftime("Successfully registered %d.%m. %H:%M" if row.n is not None and row.n <= registrations.event.seats else "Seat not available (%d.%m. %H:%M). We will make sure to provide the lecture materials online.")
+                                "Reservation deleted by admin" if row.deleted else row.time.strftime("Successfully registered %d.%m. %H:%M" if row.n is not None and row.n <= registrations.event.seats else "Seat not available (%d.%m. %H:%M). We will make sure to provide the lecture materials online."),
                             ),
                             h("td", klass="no-print")(
                                 h("form", method="POST")(
                                     h("input", type="hidden", name="name", value=row.name),
                                     h("input", type="hidden", name="restore" if row.deleted else "delete", value=registrations.event.id),
-                                    h("button")("Restore" if row.deleted else "Delete")
+                                    h("button")("Restore" if row.deleted else "Delete"),
                                 )
-                            ) if admin else None
+                            ) if admin else None,
                         ) for row in registrations.rows() if row.name == email or admin
                     ])
                 ) if admin or registrations.has(email) else None,
                 h("form", method="POST", onsubmit="return confirm('Please register only if you will physically attend this lecture.')" if not admin else None)(
                     h("input", type="text", name="name", placeholder=email) if admin else None,
                     h("input", type="hidden", name="reserve", value=registrations.event.id),
-                    h("button", type="submit")("Reserve seat (admin)" if admin else "Reserve seat")
+                    h("button", type="submit")("Reserve seat (admin)" if admin else "Reserve seat"),
                 ) if admin or not registrations.has(email) else None,
             ) for registrations in events
         ],
