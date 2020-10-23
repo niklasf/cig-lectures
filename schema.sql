3292f3d06ef1f00eef14b92828ce06ff1928c836
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-10-23 18:40:20 +0200

    create database schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+-- Registrations
+
 CREATE TABLE IF NOT EXISTS registrations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     event INTEGER NOT NULL,
@@ -8,3 +10,19 @@ CREATE TABLE IF NOT EXISTS registrations (
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_registrations_event_name ON registrations (event, name);
+
+-- Quiz
+
+CREATE TABLE IF NOT EXISTS quiz_participants (
+    quiz VARCHAR(128) NOT NULL,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_quiz_participants_quiz_name ON quiz_participants (quiz, name);
+
+CREATE TABLE IF NOT EXISTS quiz_answers (
+    id VARCHAR(32) PRIMARY KEY,
+    quiz VARCHAR(128) NOT NULL,
+    correct INTEGER NOT NULL,
+    answers TEXT NOT NULL
+);
